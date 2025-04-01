50d594d829d0ea0d669ae91481ea7b89ecea0f8f
HyunSoo.Kim <hyunsu9177@gamil.com>
2025-04-01 18:08:30 +0900

    first commit

---
+++ reactive-chat/src/main/java/com/allpick/reactivechat/ReactiveChatSystemApplication.java
@@ -0,0 +1,13 @@
+package com.allpick.reactivechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveChatSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveChatSystemApplication.class, args);
+	}
+
+}
